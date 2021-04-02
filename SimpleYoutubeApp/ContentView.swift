316a1e46d7aea2e66e91853dcc14d7bd143fea26
Fabirt <fandig27@gmail.com>
2021-04-02 08:18:46 -0500

    Initial Commit

---
+++ SimpleYoutubeApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SimpleYoutubeApp
+//
+//  Created by Fabi Diartt on 2/04/21.
+//  Copyright © 2021 Fabi Diartt. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
